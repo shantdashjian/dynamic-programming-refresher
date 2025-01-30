5026061a8eff8dadc371e266ef70b754d26d67ef
Shant Dashjian <shantgdashjian@gmail.com>
2025-01-30 14:46:19 +0400

    Add Fibonacci dynamic programming memoization solution

---
+++ fibonacci/fibonacci-dp-memoization.py
@@ -0,0 +1,15 @@
+def fib(n, memo = {}):
+    if n <= 2:
+        return 1
+    if n not in memo:
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
+
+# Complexity Analysis:
+# O(n) time | O(n) space
+
+# Test Cases:
+print(fib(1))
+print(fib(7))
+print(fib(10))
+print(fib(50))
