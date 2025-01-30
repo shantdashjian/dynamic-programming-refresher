e7284228e1bc90157cb337239bcf5937b2430c31
Shant Dashjian <shantgdashjian@gmail.com>
2025-01-30 16:11:59 +0400

    Add grid travel dynamic programming memoization solution

---
+++ grid-travel/grid-travel-dp-memoization.py
@@ -0,0 +1,16 @@
+def grid_travel(m, n, memo = {}):
+    if m == 1 or n == 1:
+        return 1
+    key = f"{min(m, n)}, {max(m, n)}"
+    if key not in memo:
+        memo[key] = grid_travel(m - 1, n) + grid_travel(m, n - 1)
+    return memo[key]
+
+# Complexity Analysis:
+# O(m + n) time | O(m + n) space
+
+# Test Cases:
+print(grid_travel(1, 1))
+print(grid_travel(2, 3))
+print(grid_travel(4, 9))
+print(grid_travel(100, 100))
