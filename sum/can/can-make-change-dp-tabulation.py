3bc230679bd2dcaa1133f010aeb8cdb7982eb986
Shant Dashjian <shantgdashjian@gmail.com>
2025-02-08 13:56:23 +0400

    Add tabulation solution for the sum problem

---
+++ sum/can/can-make-change-dp-tabulation.py
@@ -0,0 +1,18 @@
+def can_make_change(target, coins):
+    table = [False for i in range(target + 1)]
+    table[0] = True
+    for i in range(target + 1):
+        for coin in coins:
+            if table[i] and i + coin <= target:
+                table[i + coin] = True
+    return table[target]
+
+# Complexity Analysis:
+# O(m * n) time | O(m) space
+
+# Test Cases:
+print(can_make_change(6, [1, 5]))
+print(can_make_change(7, [5, 3, 4, 7]))
+print(can_make_change(2, [5, 3, 4, 7]))
+print(can_make_change(300, [7, 14]))
+    
